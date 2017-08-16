507f976c4d4d33ab8cb21c1458e07449b8c1c215
Keshaw Kumar <keeshaaw@gmail.com>
2017-08-16 16:07:49 +0530

    Added V2 of one D peak. Uses divide and conquer strategy

--- src/main/java/lecture1/OneDPeak.java
+++ src/main/java/lecture1/OneDPeak.java
@@ -2,52 +2,59 @@ package lecture1;
 
 public class OneDPeak {
 
-	static int [] a = {1, 2, 3, 5, 6, 7, 8, 9, 10, 323, 324, 895};
+	static int [] inputArray = {1, 2, 3, 5, 6, 7, 16, 9, 10, 323, 324, 895};
 	
 	//One D Peak: if a[i] >= a[i-1] and a[i] >= a[i+1] then a[i] is local peak.
 	
 	
 	//Approach 1: Go to each of the element of a and look for it's neighbors
-	public static int findPeakV1(int [] in) {
-		for(int i = 0; i < in.length; i++) {
-			if(i == 0 && in[i] >= in[i + 1]) {
-				return in[i];
-			} else if(i == in.length - 1 && in[i - 1] <= in[i]) {
-				return in[i];
-			} else {
-				if(i > 0 && i < in.length - 1 && in[i] >= in[i - 1] && in[i] >= in[i + 1]) {
-					return in[i];
-				}
+	static int findPeakV1(int [] a) {
+		for(int i = 0; i < a.length; i++) {
+			if(i == 0 && a[i + 1] <= a[i]) {
+				return i;
+			}
+			else if(i == a.length - 1 && a[i - 1] <= a[i]) {
+				return i;
+			}
+			else if(i != 0 && i < a.length - 1 && a[i] >= a[i - 1] && a[i] >= a[i + 1]) {
+				return i;
 			}
 		}
-		return 0;
+		return -1;
 	}
 	
 	/**
 	 * Approach 2: This approach uses divide and conquer strategy similar to Binary Search.
 	 * Algorithm:
-	 * 		Step 1- Select the middle element of the array i.e. a[n/2]
+	 * 		Step 1- Select the middle element of the array i.e. a[n/2]. (n is length of the input array)
 	 * 		Step 2- if a[n/2 - 1] <= a[n/2] and a[n/2 + 1] <= a[n/2] then a[n/2] is your local one D peak.
 	 * 				if a[n/2 - 1] > a[n/2] and a[n/2 + 1] <= a[n/2] go to the left and repeat the process.
 	 * 				if a[n/2 - 1] <= a[n/2] and a[n/2 + 1] > a[n/2] go to the right and repeat the process.
 	 * 				if a[n/2 - 1] > a[n/2] and a[n/2 + 1] > a[n/2] choose a random side and repeat the process.
 	 */
-	public static int findPeakV2(int [] in, int start, int end) {
-		int middle = (start + end) / 2;
-		int peak = 0;
-		if(in[middle - 1] <= in[middle] && in[middle + 1] <= in[middle]) {
-			peak = in[middle];
-		} else if(in[middle - 1] > in[middle] && in[middle + 1] <= in[middle]) {
-			findPeakV2(in, 0, middle - 1);
-		} else if(in[middle - 1] <= in[middle] && in[middle + 1] > in[middle]) {
-			findPeakV2(in, middle + 1, in.length);
+	public static int findPeakV2(int [] a, int start, int end) {
+		if(end - start == 1) {
+			if(start == 0)
+				return start;
+			else
+				return end;
 		}
-		return peak;
+		
+		int i = (start + end)/2;
+		if(a[i] < a[i - 1]) {
+			return findPeakV2(a , start, i);
+		}
+		if(a[i] < a[i + 1])
+			return findPeakV2(a, i, end);
+		else
+			return i;
 	}
 	
-	
 	public static void main(String[] args) {
-		System.out.println(findPeakV1(a));
-		System.out.println(findPeakV2(a, 0, a.length));
+		int result = findPeakV1(inputArray);
+		System.out.println("Peak Value: " + inputArray[result] + " Position: " + result);
+		
+		int res = findPeakV2(inputArray, 0, inputArray.length - 1);
+		System.out.println("Peak Value: " + inputArray[res]+ " Position: " + res);
 	}
 }
